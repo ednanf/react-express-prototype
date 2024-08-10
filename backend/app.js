8bbbcd2107b426a2af89c1fcd2c2a8ab7bf8c397
Ednan R.  Frizzera Filho <ednan@frizzera.dev>
2024-08-10 13:35:01 -0300

    Connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Mongoose
+mongoose.connect("mongodb://127.0.0.1:27017/gameLibrary");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Connected to local MongoDB"));
+
+// Server init
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
