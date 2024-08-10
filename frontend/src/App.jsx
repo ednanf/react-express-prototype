51452a458b4774332caa770acbe6574c26059d01
Ednan R.  Frizzera Filho <ednan@frizzera.dev>
2024-08-10 14:41:33 -0300

    Connect the front end to the back end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/v1/games")
+      .then((res) => res.json())
+      .then((result) => setGames(result.data.games));
+  }, []);
+
+  function renderItems() {
+    return games.map((game, index) => {
+      return (
+        <div key={index}>
+          <p>Title: {game.title}</p>
+          <p>Platform: {game.platform}</p>
+        </div>
+      );
+    });
+  }
+
+  console.log(games);
+
   return (
     <>
       <main>
         <h1>Game Collection</h1>
+        {renderItems()}
       </main>
     </>
   );
