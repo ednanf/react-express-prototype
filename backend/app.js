5986b0d09de927dc279c27615621d7910d4d8236
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-13 10:45:25 -0300

    Add Atlas connection

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const Game = require("./models/game");
+const Game = require('./models/game');
 
 const app = express();
 dotenv.config();
@@ -10,31 +10,33 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 // Mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/gameLibrary");
+const URI = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(URI);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => console.log("Connected to local MongoDB"));
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Connected to Atlas MongoDB'));
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.get("/api/v1/games", async (req, res) => {
+app.get('/api/v1/games', async (req, res) => {
   const games = await Game.find();
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
       games,
     },
   });
 });
 
-app.post("/api/v1/games", async (req, res) => {
+app.post('/api/v1/games', async (req, res) => {
   const { title, platform } = req.body;
   const game = new Game({ title, platform });
   await game.save();
-  res.status(201).send("Game created successfully!");
+  res.status(201).send('Game created successfully!');
 });
 
 // Server init
